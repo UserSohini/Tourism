65ce52c7d278a05927cd196063df7b11ee7b499e
UserSohini <bhowmicksohini2@gmail.com>
2025-04-05 20:46:34 +0530

    Add files via upload

---
+++ change.js
@@ -0,0 +1,7 @@
+const myCarouselElement = document.querySelector('#myCarousel')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch:false
+})
+// class="bd-placeholder-img card-img-top" width="100%" height="225"
